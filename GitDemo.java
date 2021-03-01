2dc55fc85c580b9a847a97e126513e7ba83c250a
haleymcmahon <72267997+haleymcmahon@users.noreply.github.com>
2021-03-01 16:39:52 -0700

    Update GitDemo.java
    added name to line 2, added num to previousNumber, created private double reverseNum, assigned newNumber to reverseNum

--- GitDemo.java
+++ GitDemo.java
@@ -1,5 +1,5 @@
 /*****
- * Edit this line to have your name.
+ * Haley McMahon
  ******/
 public class GitDemo {
     
@@ -14,6 +14,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
+        previousNumber += num;
         return 0;
     }
     
@@ -41,9 +42,11 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
+        private double reverseNum;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
+            reverseNum = newNumber;
         }
         
         return newNumber;
